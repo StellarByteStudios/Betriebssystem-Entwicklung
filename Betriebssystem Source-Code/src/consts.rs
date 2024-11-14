2a3b7da77185b924c59e8fa51477bd31d003c65f
StellarByteStudios <krollmann.carsten@gmail.com>
2024-11-14 13:54:36 +0100

    Temp-Heap Kleiner für weniger Verschnitt

--- Betriebssystem Source-Code/src/consts.rs
+++ Betriebssystem Source-Code/src/consts.rs
@@ -16,7 +16,7 @@ pub const GRAPHIC_BYTE_CLOCK_POS: (u32, u32) = ((1280 / 10) - 1, 0);
 pub const STACK_SIZE: usize = 0x1_0000;
 
 // 1 MB Heap für das Einrichten des Systems (siehe 'kmain')
-pub const TEMP_HEAP_SIZE: usize = 0x10_0000;
+pub const TEMP_HEAP_SIZE: usize = 0x1_0000;
 
 // 1 MB Heap für das Einrichten des Systems (siehe 'kmain')
 pub const KERNEL_HEAP_SIZE: usize = 0x10_0000;
