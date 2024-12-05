0c0caeab70c3d7ffe17b19cebfd6bcb8ae4423d7
StellarByteStudios <krollmann.carsten@gmail.com>
2024-12-05 12:32:57 +0100

    Aufräumen - Startup clearup

--- Betriebssystem Source-Code/src/startup.rs
+++ Betriebssystem Source-Code/src/startup.rs
@@ -22,7 +22,9 @@
 
 use core::{panic::PanicInfo, ptr};
 
+use crate::boot::multiboot::PhysRegion;
 use alloc::{string::ToString, vec};
+use boot::multiboot;
 use consts::{KERNEL_HEAP_SIZE, PAGE_FRAME_SIZE, TEMP_HEAP_SIZE};
 use devices::{cga, fonts::font_8x8, keyboard::Keyboard, pit, vga};
 use kernel::{
@@ -36,16 +38,12 @@ use kernel::{
     threads::{self, scheduler::Scheduler, sec_idle_thread},
 };
 use mylib::picturepainting::animate;
-// Funktioniert nicht mehr wegen neuer Threads
 use user::{
     applications::{self, graphic_console::graphic_console_printer, keyboard_handler},
     syscall_testthreads::{
         animation_test_thread, get_last_key_thread, get_thread_id, hello_world_thread,
         write_in_buffer_thead,
-    }, //aufgabe1::text_demo,
-       //aufgabe2::heap_demo,
-       //aufgabe3::keyboard_irq_demo,
-       //aufgabe4, aufgabe5, aufgabe6, aufgabe7,
+    },
 };
 
 extern crate alloc;
@@ -66,254 +64,61 @@ mod kernel;
 mod boot;
 mod user;
 
-fn own_tests() {
-    keyboard_handler::run();
+// Konstanten im Linker-Skript
+extern "C" {
+    static ___KERNEL_DATA_START__: u64;
+    static ___KERNEL_DATA_END__: u64;
 }
 
 fn init_all(mbi: u64) {
     kprintln!("OS initializing...");
 
-    // init allocator
-    allocator::init(allocator::HEAP_START, consts::HEAP_SIZE); // Konstruktor geändert
+    let kernel_region = get_kernel_image_region();
+    kprintln!("   kernel_region: {:?}", kernel_region);
+
+    // Verfuegbaren physikalischen Speicher ermitteln (exklusive Kernel-Image und Heap)
+    let heap_region = create_temp_heap(kernel_region.end as usize);
+    kprintln!("kmain, heap: {:?}", heap_region);
 
     // Multiboot-Infos für Grafik auslesen, falls vorhanden
     check_graphics_mode(mbi);
 
-    // init interrupts
+    // Verfuegbaren physikalischen Speicher ermitteln (exklusive Kernel-Image und Heap)
+    let phys_mem = multiboot::get_free_memory(mbi, kernel_region, heap_region);
+    kprintln!("kmain, free physical memory: {:?}", phys_mem);
+
+    // Multiboot-Infos ausgeben
+    //multiboot::dump(mbi);
+
+    // Page-Frame-Management einrichten
+    frames::pf_init(phys_mem);
+
+    // Paging fuer den Kernel aktivieren
+    let pml4_addr = pages::pg_init_kernel_tables(mbi);
+    pages::pg_set_cr3(pml4_addr);
+
+    // Nochmal richtig Kernal-Heap initialisieren
+    // Nicht sicher ob das noch nach dem Paging so läuft
+    ini_kernel_heap();
+
+    // Interrupt-Strukturen initialisieren
     interrupts::init();
 
-    // register keyboard ISR
+    // Trapgate initialisieren
+    syscall::syscall_dispatcher::init();
+
+    // Tastatur-Unterbrechungsroutine 'einstoepseln'
     Keyboard::plugin();
 
-    // Timer Interupt registrieren
+    // Zeitgeber-Unterbrechungsroutine 'einstoepseln'
     pit::plugin();
 
-    // CPU enable ints
-    cpu::enable_int();
-
-    // Clear Screen
-    cga::clear();
+    // Bildschirm frei machen
+    graphic_console_printer::clear_screen();
 
     kprintln!("Initializing finished!");
 }
 
-/*
-fn aufgabe1() {
-    //cga::clear();
-    text_demo::run();
-    kprintln!("Textdemo run");
-    //keyboard_demo::run();
-}
-
-fn aufgabe2() {
-    heap_demo::run();
-    //cga::clear();
-    //sound_demo::run();
-}
-
-fn aufgabe3() {
-    cga::clear();
-
-    /*
-    pic::forbid(IRQ_KEYBOARD);
-    pic::forbid(IRQ_TIMER);
-
-    kprintln!("Beide Interrupts sind jetzt deaktiviert");
-    kprintln!("Status Keyboard {}", pic::status(IRQ_KEYBOARD));
-    kprintln!("Status Timer {}", pic::status(IRQ_TIMER));
-
-    pic::allow(IRQ_KEYBOARD);
-    pic::allow(IRQ_TIMER);
-
-    kprintln!("Beide Interrupts sind jetzt wieder aktiviert");
-    kprintln!("Status Keyboard {}", pic::status(IRQ_KEYBOARD));
-    kprintln!("Status Timer {}", pic::status(IRQ_TIMER));
-     */
-
-    // Cursor muss in Keyboard::KeyboardISR::trigger festgesetzt werden!!!
-    keyboard_irq_demo::run();
-}
-
-fn aufgabe4() {
-    cga::clear();
-
-    // Lied abspielen
-    //pcspk::alle_meine_entchen();
-    //pcspk::starwars_imperial();
-    //pcspk::super_mario();
-    //pcspk::doom();
-    //pcspk::tetris();
-
-    // Threads Initialisieren
-    //init_all_threads();
-
-    // Scheduler aufsetzen
-    //Scheduler::schedule();
-
-    //aufgabe4::corouts_demo::run();
-    //aufgabe4::queue_tests::run();
-}
-
-fn aufgabe5() {
-    cga::clear();
-
-    // Threads Initialisieren
-    init_all_threads_preempt();
-
-    // Scheduler aufsetzen
-    Scheduler::schedule();
-}
-
-fn aufgabe6() {
-    cga::clear();
-
-    // Threads Initialisieren
-    init_all_threads_sync();
-
-    // Scheduler aufsetzen
-    Scheduler::schedule();
-}
-
-fn aufgabe7() {
-    threads::idle_thread::init();
-    /*
-    applications::graphic_console::gc_programms::clock::init(vec![
-        "clock".to_string(),
-        "start".to_string(),
-    ]); */
-    aufgabe7::test_console::init();
-
-    // Scheduler aufsetzen
-    Scheduler::schedule();
-}
-
-fn init_all_threads() {
-    threads::idle_thread::init();
-    aufgabe4::hello_world_thread::init();
-    //applications::music_thread::init();
-    //aufgabe4::coop_thread_loop::init();
-    //aufgabe4::coop_thread_demo::init();
-}
-
-fn init_all_threads_preempt() {
-    threads::idle_thread::init();
-    aufgabe4::hello_world_thread::init();
-    aufgabe5::thread_demo::init();
-    //applications::music_thread::init();
-}
-
-fn init_all_threads_sync() {
-    threads::idle_thread::init();
-    aufgabe6::semaphore_launch_thread::init();
-    //applications::music_thread::init();
-}
-
-fn print_main_screen() {
-    cga::clear();
-    println!("Byte OS: 1.0");
-    println!("------------------------------------\n");
-    println!("Aktuelle Funktionalitaeten:");
-    print!("    Bildschirmausgabe ");
-    cga::set_attribute(cga::Color::Blue, cga::Color::Yellow, true);
-    println!("(auch bunt)");
-    cga::set_default_attribute();
-    println!("    Heapverwaltung (mit Freispeicherliste)");
-    println!("    Interrupts");
-    println!("    Tastatureingabe (Ueber Interrupts)");
-    println!("    Koroutinen (Kooperativ - verkettet)");
-    println!("    Queue (Für die Threads)");
-    println!("    Scheduler (Kooperativ)");
-    println!("    Threads (Kooperativ)");
-    println!("    Musik");
-}
-
-fn print_main_graphic() {
-    let text_h = font_8x8::CHAR_HEIGHT;
-
-    vga::draw_string(0, 0, vga::rgb_24(0, 255, 0), "Byte OS: 1.0");
-    vga::draw_string(
-        0,
-        text_h,
-        vga::rgb_24(0, 255, 0),
-        "------------------------------------\n",
-    );
-    vga::draw_string(
-        0,
-        text_h * 2,
-        vga::rgb_24(0, 255, 0),
-        "Aktuelle Funktionalitaeten:",
-    );
-    vga::draw_string(
-        0,
-        text_h * 3,
-        vga::rgb_24(0, 255, 0),
-        "    - Bildschirmausgabe ",
-    );
-    vga::draw_string(
-        0,
-        text_h * 4,
-        vga::rgb_24(0, 255, 0),
-        "    - Heapverwaltung (mit Freispeicherliste)",
-    );
-    vga::draw_string(0, text_h * 5, vga::rgb_24(0, 255, 0), "    - Interrupts");
-    vga::draw_string(
-        0,
-        text_h * 6,
-        vga::rgb_24(0, 255, 0),
-        "    - Tastatureingabe (Ueber Interrupts)",
-    );
-
-    vga::draw_string(
-        0,
-        text_h * 7,
-        vga::rgb_24(0, 255, 0),
-        "    - Koroutinen (Kooperativ - verkettet)",
-    );
-    vga::draw_string(
-        0,
-        text_h * 8,
-        vga::rgb_24(0, 255, 0),
-        "    - Queue (Für die Threads)",
-    );
-    vga::draw_string(
-        0,
-        text_h * 9,
-        vga::rgb_24(0, 255, 0),
-        "    - Scheduler (Kooperativ)",
-    );
-    vga::draw_string(0, text_h * 10, vga::rgb_24(0, 255, 0), "    - Musik");
-    vga::draw_string(
-        0,
-        text_h * 11,
-        vga::rgb_24(0, 255, 0),
-        "    - Shellfunktionalität",
-    );
-    vga::draw_string(
-        0,
-        text_h * 12,
-        vga::rgb_24(34, 80, 200),
-        "        * Ein und ausgabe von Text",
-    );
-    vga::draw_string(
-        0,
-        text_h * 13,
-        vga::rgb_24(34, 80, 200),
-        "        * Auswahl von Musik",
-    );
-    vga::draw_string(
-        0,
-        text_h * 14,
-        vga::rgb_24(34, 80, 200),
-        "        * Fraktalberechnung",
-    );
-    vga::draw_string(
-        0,
-        text_h * 15,
-        vga::rgb_24(34, 80, 200),
-        "        * und vieles mehr ...",
-    )
-}
- */
 // Pruefen, ob wir in einem Grafikmodus sind
 // Falls ja setzen der Infos in VGA
 fn check_graphics_mode(mbi: u64) -> bool {
@@ -337,80 +142,6 @@ fn check_graphics_mode(mbi: u64) -> bool {
     true
 }
 
-/* Ursprüngliche Funktion, welche beim Startup lief */
-#[no_mangle]
-pub extern "C" fn startup(mbi: u64) {
-    kprintln!("OS startup...");
-
-    init_all(mbi);
-
-    //print_main_screen();
-    //print_main_graphic();
-    //kprintln!("Die Aktuelle Zeit: {}", rtc::get_time());
-    //draw_newton();
-
-    //pcspk::intro();
-
-    //input::wait_for_return();
-
-    cga::clear();
-
-    //aufgabe1();
-    //aufgabe2();
-    //aufgabe3();
-    //aufgabe4();
-    //aufgabe5();
-    //aufgabe6();
-    //aufgabe7();
-
-    own_tests();
-
-    kprintln!(" = = Closing OS = =");
-
-    loop {
-        //let mut code = key_hit();
-
-        //keyboard_handler::handle_keystroke(code.get_ascii());
-    }
-}
-
-/* Neuer Startupblock
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*/
-
-// Konstanten im Linker-Skript
-extern "C" {
-    static ___KERNEL_DATA_START__: u64;
-    static ___KERNEL_DATA_END__: u64;
-}
-
-use crate::boot::multiboot::PhysRegion;
-use boot::multiboot;
 // Start- und Endadresse des Kernel-Images ermitteln,
 // aufrunden auf das naechste volle MB und zurueckgeben
 fn get_kernel_image_region() -> multiboot::PhysRegion {
@@ -510,56 +241,6 @@ fn ph_allocator_testing() {
 
     vprintln!("\n= = = Jetzt sollte wieder der User-Space sein wie vorher = = =");
     dump_user_frames();
-}
-
-#[no_mangle]
-pub extern "C" fn kmain(mbi: u64) {
-    kprintln!("kmain");
-
-    let kernel_region = get_kernel_image_region();
-    kprintln!("   kernel_region: {:?}", kernel_region);
-
-    // Verfuegbaren physikalischen Speicher ermitteln (exklusive Kernel-Image und Heap)
-    let heap_region = create_temp_heap(kernel_region.end as usize);
-    kprintln!("kmain, heap: {:?}", heap_region);
-
-    // Multiboot-Infos für Grafik auslesen, falls vorhanden
-    check_graphics_mode(mbi);
-
-    // Verfuegbaren physikalischen Speicher ermitteln (exklusive Kernel-Image und Heap)
-    let phys_mem = multiboot::get_free_memory(mbi, kernel_region, heap_region);
-    kprintln!("kmain, free physical memory: {:?}", phys_mem);
-
-    // Multiboot-Infos ausgeben
-    multiboot::dump(mbi);
-
-    // Page-Frame-Management einrichten
-    frames::pf_init(phys_mem);
-
-    // Paging fuer den Kernel aktivieren
-    let pml4_addr = pages::pg_init_kernel_tables(mbi);
-    pages::pg_set_cr3(pml4_addr);
-
-    // Nochmal richtig Kernal-Heap initialisieren
-    // Nicht sicher ob das noch nach dem Paging so läuft
-    ini_kernel_heap();
-
-    // Interrupt-Strukturen initialisieren
-    interrupts::init();
-
-    // Trapgate initialisieren
-    syscall::syscall_dispatcher::init();
-
-    // Tastatur-Unterbrechungsroutine 'einstoepseln'
-    Keyboard::plugin();
-
-    // Zeitgeber-Unterbrechungsroutine 'einstoepseln'
-    pit::plugin();
-
-    // Grafik funktioniert grad noch nicht wegen mapping
-
-    // Bildschirm frei machen
-    graphic_console_printer::clear_screen();
 
     /*
     // Test für nullpointer exception
@@ -569,6 +250,14 @@ pub extern "C" fn kmain(mbi: u64) {
     unsafe {
         let var = *nullpointer;
     } */
+}
+
+#[no_mangle]
+pub extern "C" fn kmain(mbi: u64) {
+    kprintln!("kmain");
+
+    // Alles Wichtige Initialisieren
+    init_all(mbi);
 
     //ph_allocator_testing();
 
