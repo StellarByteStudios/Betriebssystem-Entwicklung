f0aac41233330fcebfe0e36d3ad961ad1e6f4c84
StellarByteStudios <krollmann.carsten@gmail.com>
2025-06-04 11:58:59 +0200

    Update: Der Command-Parser kann jetzt Argumente in "Quotes" als zusammenhängende Argumente erkennen

--- Betriebssystem Source-Code/os/src/kernel/shell/command_parser.rs
+++ Betriebssystem Source-Code/os/src/kernel/shell/command_parser.rs
@@ -63,11 +63,16 @@ pub fn parse_command(command: String) -> (String, Vec<String>) {
     // Programm Name rausholen
     let main_command = command_array.get(0).unwrap().clone();
 
+    // TODO: Argumente mit "" und Leerzeichen zusammenfassen
+    let merged_command_array = merge_quoted_args(command_array);
+
     // Environmentvariablen ersetzen
-    let updated_command_array = command_array
+    let updated_command_array = merged_command_array
         .into_iter()
         .map(map_string_to_env_var)
         .collect();
+
+
 
     // Variablen wieder zurückgeben
     return (main_command.to_string(), updated_command_array);
@@ -79,7 +84,7 @@ pub fn map_string_to_env_var(var: String) -> String {
         return var;
     }
 
-    kprint!("Environment Variable von {} ersetzt zu: ", var);
+    kprintln!("Environment Variable von {} ersetzt zu: ", var);
     // Variablenname rausschneiden
     let new_var = var.replace('$', "");
 
@@ -96,4 +101,70 @@ pub fn map_string_to_env_var(var: String) -> String {
 
     // Variablenkontent auspacken und zurückgeben
     return env_var.unwrap();
+}
+
+
+/* = = Funktion zum mergen der Argumente in "Quotes"  = = */
+fn merge_quoted_args(args: Vec<String>) -> Vec<String> {
+    let mut result = Vec::new();
+    let mut in_quotes = false;
+    let mut current = String::new();
+
+    // alle Argumente durchgehen
+    for arg in args {
+
+        // Sind wir grade in einem zusammengesetzten Argument?
+        if in_quotes {
+            // Gelöschtes Leerzeichen hinzufügen
+            current.push(' ');
+            // Neues Argument an String anhängen
+            current.push_str(&arg);
+
+            // Sind wir am ende der Quots
+            if arg.ends_with('"') {
+                // Zusammensetzung beenden
+                in_quotes = false;
+                // Quotationmark am Ende löschen
+                let merged = current.trim_matches('"').to_string();
+                // Als neues zusammengefügtes Argument in Vektor inzufügen
+                result.push(merged);
+                // String wieder leer machen für die nächsten Quotes
+                current.clear();
+            }
+
+            continue; // Skipt, da wir arg ja bearbeitet haben
+        }
+
+        // Beginnt hier ein zusammengesetztes Wort?
+        if arg.starts_with('"') {
+
+            // Wenn es direkt wieder damit Endet, brauchen wir nix zu mergen
+            if arg.ends_with('"') && arg.len() > 1 {
+                // Abschneiden der Quotes auf beiden Seiten
+                let merged = arg.trim_matches('"').to_string();
+                // Direkt hinzufügen zu den Argumenten
+                result.push(merged);
+
+                continue; // Skipt, da wir arg ja bearbeitet haben
+            }
+            // Markierung, das wir jetzt ein zusammengesetztes Argument einlesen
+            in_quotes = true;
+            // Argument in unseren zusammengesetzten String packen
+            current.push_str(&arg);
+
+            continue; // Skip pushing to result again
+        }
+
+        // Normales Argument einfach in den Vektor schreiben
+        result.push(arg);
+    }
+
+    // Optional: Fehlerbehandlung, falls ein Anführungszeichen nicht geschlossen wurde
+    if in_quotes {
+        // Einfach den bisher zusammengesetzten String noch in die Argumente packen
+        result.push(current);
+    }
+
+    // Neuer Argumenten Vektor
+    return result;
 }
